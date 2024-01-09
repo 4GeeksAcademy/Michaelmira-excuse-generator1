5a249a291d51c45f9f6d5a01de4cb52e557edffa
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-01-09 04:54:25 +0000

    add excuse function generateExcuse and list of excuses

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let generateExcuse = () => {
+    let pronown = ["A", "The"];
+  let subject = [
+    "some guy",
+    "my neighbor ",
+    "dog",
+    "some asshole on the street",
+    "wife",
+    "girlfriend"
+  ];
+  let action = ["stole my", "kicked my", "ate my", "drove over my"];
+  let possession = ["homework", "car", "girlfriend", "shoes"];
+  let where = [
+    "on the freeway",
+    "at my place",
+    "in the pool",
+    "at my girfriends house",
+    "on top of my car and i started driving"
+  ];
 };
